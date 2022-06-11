9b716ea8dacf889f95360ce10f06430c6cc00174
arnobtarafder <rmtarnob@gmail.com>
2022-06-11 20:31:18 +0600

    add portflio slider

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+
+      <h1 className='text-green-500 font-sans text-center'>Hello Portfolio</h1>      
 
     </div>
   );
