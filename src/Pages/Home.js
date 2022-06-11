dd81a3bc5266f4bc91a615caff4ef7f8688e8b09
arnobtarafder <rmtarnob@gmail.com>
2022-06-11 20:40:50 +0600

    add: navigation bar

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import About from '../Components/Home/About/About';
+import Banner from '../Components/Home/Banner/Banner';
+import Contact from '../Components/Home/Contact/Contact';
+import Portfolio from '../Components/Home/Portfolio/Portfolio';
+import Services from '../Components/Home/Services/Services';
 
 const Home = () => {
     return (
         <div>
-            
+            <Banner></Banner>
+            <About></About>
+            <Services></Services>
+            <Portfolio></Portfolio>
+            <Contact></Contact>
         </div>
     );
 };
